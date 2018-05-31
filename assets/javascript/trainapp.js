e114568e269f5173788ab46619cb2ddcac81f527
sblapray1014 <36177384+sblapray1014@users.noreply.github.com>
2018-05-31 15:05:33 -0600

    Most functionality and framework built out, need to figure out momentjs

---
+++ assets/javascript/trainapp.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+    var config = {
+        apiKey: "AIzaSyBkOX_jQsWzMqZnZSIe-tkNw6rlLbaKUA8",
+        authDomain: "trainapp-b7188.firebaseapp.com",
+        databaseURL: "https://trainapp-b7188.firebaseio.com",
+        projectId: "trainapp-b7188",
+        storageBucket: "",
+        messagingSenderId: "942653445503"
+    };
+    firebase.initializeApp(config);
+    console.log("I am loaded!");
+    var database = firebase.database();
+
+    $(".btn").on("click", function (event) {
+        event.preventDefault();
+        var train = $("#train-input").val().trim();
+        var destination = $("#destination-input").val().trim();
+        var trainTime = $("#first-input").val().trim();
+        var freq = $("#freq-input").val().trim();
+
+        var newRow = $("<tr>");
+        var newTd1 = $("<td>").text(train);
+        var newTd2 = $("<td>").text(destination);
+        var newTd3 = $("<td>").text(freq);
+        var newTd4 = $("<td>").text("hello");
+        var newTd5 = $("<td>").text("hello");
+
+        newRow.append(newTd1, newTd2, newTd3, newTd4, newTd5);
+        $("tbody").append(newRow);
+
+
+        var newTrain = {
+            Train: train,
+            Destination: destination,
+            FirstTime: trainTime,
+            Freq: freq
+        }
+        database.ref().push(newTrain);
+
+        console.log(newTrain.Train);
+        console.log(newTrain.Destination);
+        console.log(newTrain.FirstTime);
+        console.log(newTrain.Freq);
+
+        $("#train-input").val("");
+        $("#destination-input").val("");
+        $("#first-input").val("");
+        $("#freq-input").val("");
+
+        database.ref().on("child_added", function (childSnapshot, prevChild) {
+            console.log(childSnapshot.val());
+
+            var train = childSnapshot.val().Train;
+            var destination = childSnapshot.val().Destination;
+            var trainTime = childSnapshot.val().FirstTime;
+            var freq = childSnapshot.val().Freq;
+        });
+    });
+
+});
